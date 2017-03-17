f5f1b4ecadd8bd296fe0b2d3e26f0c3696d94441
sebastiaans10 <sebastiaans10@gmail.com>
2017-03-17 18:02:44 +0100

    custom directory clock

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 var shopApp = angular.module('shopApp', ['ngRoute']);
 
+shopApp.directive('myClock', myClock);
+
+function myClock(){
+  var directive = {
+    template: '<div>{{vm.time | date:"HH:mm:ss"}}</div>',
+    controller: clockController,
+    controllerAs: 'vm'
+      };
+      return directive;
+}
+
+clockController.$inject=['$interval'];
+function clockController($interval){
+  var vm = this;
+  var update = $interval(function(){
+      vm.time = new Date();
+  }, 1000);
+
+}
+
 shopApp.config(['$routeProvider', function($routeProvider) {
 
 
     $routeProvider.when('/home', {
             templateUrl: '/views/home.html',
-            controller: 'shopController'
+            controller: 'shopController',
+            directive: 'myClock'
         })
         .when('/productList', {
             templateUrl: '/views/productList.view.html',
