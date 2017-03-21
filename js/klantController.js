639493c031e009b7b6a39b460c785f96aeefcb95
jvhoven <jeffreyvanhoven@gmail.com>
2017-03-21 23:39:55 +0100

    localStorage geimplementeerd voor klanten.

--- js/klantController.js
+++ js/klantController.js
@@ -1,125 +1,33 @@
-//var module = angular.module('klant', []);
+shopApp.controller('klantController', ['klantService', '$scope', '$location',
+    function (klantService, $scope, $location) {
+        $scope.klanten = klantService.getKlanten();
 
+        $scope.saveNewKlant = function () {
+            var newPerson = {
+                name: $scope.newKlantItem.name,
+                adres: $scope.newKlantItem.adres,
+                email: $scope.newKlantItem.email
+            };
 
+            klantService.addKlant(newPerson, (data) => {
+                $scope.klanten = data;
+                $scope.newKlantItem = {};
+            });
 
-  shopApp.controller('klantController', ['klantService', '$scope', '$location',
-  function (klantService, $scope, $location){
-  //  $scope.vm = {};
+            $location.path('/klantLijst');
+        };
 
-  //  var localItems = JSON.parse(localStorage.getItem("klanten"));
+        $scope.deleteKlant = function (klantItem) {
+            klantService.deleteKlant(klantItem, (data) => {
+                $scope.klanten = data;
+            });
+        };
 
-    $scope.klanten = klantService.getKlanten();
+        $scope.updateKlant = function (klantItem) {
+            klantItem.updating = false;
+            klantService.updateKlant(klantItem, (data) => {
+                $scope.klanten = data;
+            });
+        };
 
-
-  /*  if(localItems != undefined && localItems.length>0){
-    $scope.vm = localItems;
-    }
-    else{
-      $scope.vm.klanten = klantService.getKlanten();
-
-    }*/
-
-    $scope.saveNewKlant = function(){
-
-      var newPerson = {
-        name:   $scope.newKlantItem.name,
-        adres:  $scope.newKlantItem.adres,
-        email:  $scope.newKlantItem.email
-      };
-      //  klantService.addKlant(newPerson);
-    //  var klantenList = $scope.klanten;
-      klantService.addKlant(newPerson);
-      //  $scope.klanten.push(newPerson);
-      $scope.newKlantItem = {};
-    	//localStorage.setItem("klanten", JSON.stringify($scope.klanten));
-       $location.path('/klantLijst');
-
-    };
-
-    $scope.deleteItem = function(klantItem) {
-
-      klantService.deleteKlant(klantItem);
-
-
-  		//$scope.klanten.splice(deleteIndex, 1);
-
-    //  $scope.klanten.splice(deleteIndex, 1);
-    //  localStorage.setItem("klanten", JSON.stringify($scope.klanten));
-
-    		};
-    //console.log($scope.vm);
-    $scope.updateItem = function(klantItem) {
-      klantItem.updating=false;
-      klantService.updateKlant(klantItem);
-
-  //    localStorage.setItem("klanten", JSON.stringify($scope.klanten));
-    };
-  }]);
-  //klantController.$inject = ['klantService', '$location'];
-
-
-
-
-/*angular.module('shopApp').controller('klantController', function($scope, klantFactory){
-
-var localKlantItems = JSON.parse(localStorage.getItem("klantItems"));
-
-if(localKlantItems != undefined && localKlantItems.length>0){
-$scope.klantItems = localKlantItems;
-}
-
-//else{
-  $scope.klantItems = klantFactory.getKlanten();
-  /*[
-    { id:"0",
-      name:"Robert van Veen",
-      adress:"Rietstraat 5",
-      email:"robvveen@gmail.com"
-    },
-    { id:"1",
-      name:"Frank Andersen",
-      adress:"Molenstraat 3",
-      email: "frankandersen@gmail.com"
-    }
-  ];
-//}
-
-$scope.deleteItem = function(klantItem) {
-
-		var deleteIndex = -1;
-		$scope.klantItems.forEach(function(e, i) {
-			if(e.id === klantItem.id) {
-				deleteIndex = i;
-			}
-		});
-
-		$scope.klantItems.splice(deleteIndex, 1);
-
-		localStorage.setItem("klantItems", JSON.stringify($scope.klantItems));
-	};
-
-	$scope.saveNewItem = function() {
-		console.log($scope.newKlantItem);
-
-		$scope.klantItems.push({
-      id: $scope.newKlantItem.id,
-			name: $scope.newKlantItem.name,
-      adress: $scope.newKlantItem.adress,
-      email: $scope.newKlantItem.email
-		});
-
-		$scope.newKlantItem = {};
-
-		localStorage.setItem("klantItems", JSON.stringify($scope.klantItems));
-    window.location.href = '#!/klantLijst';
-
-	};
-
-	$scope.updateItem = function(klantItem) {
-		klantItem.updating=false;
-
-		localStorage.setItem("klantItems", JSON.stringify($scope.klantItems));
-	};
-
-});
-*/
+    }]);
