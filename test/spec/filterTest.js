083250b4a08c8c3ccbe294326daaab4a880f2e23
sebastiaans10 <sebastiaans10@gmail.com>
2017-03-27 15:03:08 +0200

    10 werkende unittests

--- test/spec/filterTest.js
+++ test/spec/filterTest.js
@@ -1,4 +1,3 @@
-
   describe('length filter', function() {
     beforeEach(angular.mock.module('shopApp'));
 
@@ -8,13 +7,38 @@ beforeEach(inject(function(_$filter_){
   $filter = _$filter_('priceGreaterThan');
 }));
 
-it('returns 0 when given null', function() {
-/*  var length = $filter('length');
-  expect(length(null)).toEqual(0);*/
+it('returns all products with price greater than', function() {
+    var input =
+    [
+      {id: 1, name: 'Voetbal', price: '30', description:'mooi', image: 'img/voetbal.png', voorraad: '4'},
+      {id: 2, name: 'Tennis racket', price: '75', description: 'beter', image:'img/tennis.png', voorraad:'3'},
+      {id: 3, name: 'Piano', price: '1500', description: 'vleugel', image:'img/piano.png', voorraad:'3'}];
+
+      var output = $filter(input, 75);
+      expect(output.length).toBe(2);
+      expect(output[0].name).toEqual('Tennis racket');
+      expect(output[1].name).toEqual('Piano');
 });
 
-it('returns the correct value when given a string of chars', function() {
-/*  var length = $filter('length');
-  expect(length('abc')).toEqual(3);*/
+it('returns all products when price is undefined', function() {
+  var input =
+  [
+    {id: 1, name: 'Voetbal', price: '30', description:'mooi', image: 'img/voetbal.png', voorraad: '4'},
+    {id: 2, name: 'Tennis racket', price: '75', description: 'beter', image:'img/tennis.png', voorraad:'3'},
+    {id: 3, name: 'Piano', price: '1500', description: 'vleugel', image:'img/piano.png', voorraad:'3'}];
+
+    var output = $filter(input, undefined);
+    expect(output.length).toBe(3);
+});
+
+it('returns all products when price is empty', function() {
+  var input =
+  [
+    {id: 1, name: 'Voetbal', price: '30', description:'mooi', image: 'img/voetbal.png', voorraad: '4'},
+    {id: 2, name: 'Tennis racket', price: '75', description: 'beter', image:'img/tennis.png', voorraad:'3'},
+    {id: 3, name: 'Piano', price: '1500', description: 'vleugel', image:'img/piano.png', voorraad:'3'}];
+
+    var output = $filter(input, "");
+    expect(output.length).toBe(3);
 });
 });
