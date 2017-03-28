08d20a37713b6761d4ea792f214230bb1742aad4
sebastiaans10 <sebastiaans10@gmail.com>
2017-03-28 11:11:48 +0200

    E2E test product toevoegen

---
+++ test/e2e/controllers/shopTest.js
@@ -0,0 +1,24 @@
+describe('Test product toevoegen', function() {
+
+    beforeEach(function() {
+        browser.get('http://localhost:8080/#!/addProduct');
+
+    });
+
+    it('Boek toevoegen', function() {
+        var productList = browser.executeScript("return JSON.parse(localStorage.getItem('producten'));");
+        expect(productList.length).toBe(undefined);
+        element(by.model('newProductItem.name')).sendKeys("Piano");
+        element(by.model('newProductItem.price')).sendKeys("1500");
+        element(by.model('newProductItem.description')).sendKeys("Mooie vleugel");
+        element(by.model('newProductItem.image')).sendKeys("img/piano.png");
+        element(by.model('newProductItem.voorraad')).sendKeys("2");
+
+        element(by.id('addP')).click();
+
+        productList = browser.executeScript("return JSON.parse(localStorage.getItem('producten'));");
+
+        expect(productList.length).not.toEqual(0);
+    });
+
+});
